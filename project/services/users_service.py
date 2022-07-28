49fc62d468d538c75c071f3b87b85505a8b0d7e3
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-07-28 16:42:19 +0500

    final

--- project/services/users_service.py
+++ project/services/users_service.py
@@ -4,7 +4,7 @@ from project.dao import UsersDAO
 
 from project.exceptions import ItemNotFound
 from project.models import User
-from project.tools.security import generate_tokens, approve_refresh_token, get_data_from_token
+from project.tools.security import generate_tokens, approve_refresh_token, get_data_from_token, generate_password_hash
 
 
 class UsersService:
@@ -47,5 +47,5 @@ class UsersService:
     def update_password(self, data, refresh_token):
         user = self.get_by_user_token(refresh_token)
         if user:
-            self.dao.update(login=user.email, data={"password": generate_tokens(data.get('password_2'))})
-            data = self.check(login=user.email, password=data.get('password_2'))
+            self.dao.update(login=user.email, data={"password": generate_password_hash(data.get('password_2'))})
+            return self.check(login=user.email, password=data.get('password_2'))
