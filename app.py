cbdcaa0326b96d72db9fb494cb88f7a997b3874a
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-07-28 15:24:21 +0500

    final front

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_restx import Api
+
+from config import Config
+from setup_db import db
+from views import directors_ns
+
+api = Api(title="Flask Course Project 3", doc="/docs")
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    db.init_app(app)
+    api.init_app(app)
+
+    api.add_namespace(directors_ns)
+
+    return app
