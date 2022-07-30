8e454dd4727f7c0039e2ce1f2fe16fed69d99fcc
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-07-30 16:38:09 +0500

    final redact 1

--- project/server.py
+++ project/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 from project.exceptions import BaseServiceError
@@ -14,6 +14,10 @@ def base_service_error_handler(exception: BaseServiceError):
 def create_app(config_obj):
     app = Flask(__name__)
     app.config.from_object(config_obj)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     CORS(app=app)
     db.init_app(app)
--- run.py
+++ run.py
@@ -2,10 +2,10 @@ from project.config import config
 from project.models import Genre, User, Director, Movie
 from project.server import create_app, db
 
-app = create_app(config)
+appl = create_app(config)
 
 
-@app.shell_context_processor
+@appl.shell_context_processor
 def shell():
     return {
         "db": db,
@@ -15,8 +15,9 @@ def shell():
         "Movie": Movie
     }
 
+
 if __name__ == '__main__':
-    app.run(
+    appl.run(
         host='localhost',
         port=25000,
         debug=True
